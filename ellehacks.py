1c575c09e5febf7fd273a105bbf16dcd62056ccf
JenishaT <36098623+JenishaT@users.noreply.github.com>
2019-02-03 06:22:32 -0500

    Update ellehacks.py

--- ellehacks.py
+++ ellehacks.py
@@ -9,6 +9,11 @@ from tutor import Tutor
 tutees = [] 
 tutors = []
 
+def logout():
+    print("Thanks for working with us!")
+    print("Logging Out........\n\n")
+    homepage()
+    
 def change_username():
     print("Please choose your new username.")
     username = input("")
@@ -35,7 +40,7 @@ def set_username_and_password():
     return full_name, username, password
 
 def set_grade_level():
-    print("Please indicate your current level of education. \nType 'P' for primary (grades 1 - 8),\n'S' for secondary (grades 9 - 12), \n 'PS' for post secondary (university/college level education)")
+    print("Please indicate your current level of education. \nType 'P' for primary (grades 1 - 8),\n'S' for secondary (grades 9 - 12), \n'PS' for post secondary (university/college level education)")
     while True:
         education_level = input("").lower().strip()
         if education_level == 'p' or education_level == 's' or education_level == 'ps':         
@@ -71,11 +76,11 @@ def set_location():
 def set_sex():
     while True:
         response = input("").lower().strip()        
-        if response == 'f' or response == 'm' or response == 'd':
+        if response == 'f' or response == 'm' or response == 'e':
             sex = response
             break
         else:
-            print("Sorry, please enter either 'F', 'M', or 'D'")  
+            print("Sorry, please enter either 'F', 'M', or 'E'")  
     return sex
 
 def set_subjects():
@@ -128,11 +133,22 @@ def set_cost():
         print("Please enter number values!")
         cost = input("$")    
     return float(cost)
-def display_tutors(sorted_tutors):
-    pass
 
-    
+def get_tutor_data(searchname, tutors):
+    data = ""
+    for tutor in tutors:
+        if tutor.username == searchname:
+            data = "Name: " + tutor.full_name + "\nEducation: " + tutor.education_level+ "\nGrade: " + str(tutor.grade_year) + "\nSubjects: " + str(tutor.subjects)
+            return data
+        
+def display_tutors(sorted_tutors):
+    for name in sorted_tutors:
+        print(get_tutor_data(name, tutors))
+        print("------------------------------------")
+
 def order_tutors(tutee, tutors):
+    print("Here are you are your matches:")
+    print("------------------------------------")
     matches = {}
     for tutor in tutors:
         matches[tutor.username] = 0
@@ -147,11 +163,18 @@ def order_tutors(tutee, tutors):
             if tutor.weboption == tutee.weboption:
                 matches[tutor.username] += 1
     sorted_tutors = sorted(matches, key=matches.get, reverse=True)
-    display_tutors(sorted_tutors, matches)
+    display_tutors(sorted_tutors)
+    print("Type 'R' to go return to the previous screen")
+    while True:
+        response = input("").lower().strip()        
+        if response == 'r':
+            welcome_tutee(tutee)
+        else:
+            print("Please enter 'R', to go back")      
     
 def welcome_tutee(tutee):
-    print("Welcome,", tutee.full_name, ". Would you like to review your profile information?")
-    print("Type 'R' if you would like to review your profile, or type 'B' to begin looking for tutors.")
+    print("Welcome,", tutee.full_name, "Would you like to review your profile information?")
+    print("Type 'R' if you would like to review your profile, or type 'B' to begin looking for tutors.\nType 'L' to logout")
     x = True
     while x:
         response = input("").lower().strip()
@@ -171,28 +194,40 @@ def welcome_tutee(tutee):
             print("Weboption:", tutee.weboption)
             print("Min price:", tutee.min_price)
             print("Max price:", tutee.max_price)
-            print("If you would like to change any of the information shown above, type 'Y', or otherwise, type 'N'.")
-            while True:
-                response = input("").lower().strip()
-                if response == 'y':
-                    #change_information()
-                    x = False
-                    break
-                elif response == 'n':                    
-                    #find_tutors()
-                    x = False
-                    break
-                print("Sorry, please enter either 'Y' or 'N'.")
+            
+            print("Type 'B' to begin looking for tutors.\n Type 'L' to logout")
+            response = input("").lower().strip()
+            if response == 'b':
+                order_tutors(tutee, tutors)
+                break
+            elif response == 'l':
+                logout()
+            else:
+                print("Sorry, please enter either 'B' or 'L'")            
+            
+            # functions to change specific parameters
+            #print("If you would like to change any of the information shown above, type #'Y', or otherwise, type 'N'.")
+            #while True:
+            #    response = input("").lower().strip()
+            #    if response == 'y':
+            #        #change_information()
+            #        x = False
+            #        break
+            #    elif response == 'n':                    
+            #        order_tutors()
+            #        x = False
+            #       break
+            #    print("Sorry, please enter either 'Y' or 'N'.")
             break
         elif response == 'b':
-            #find_tutors()
+            order_tutors(tutee, tutors)
             break
         else:
-            print("Sorry, please enter either 'R' or 'B'.")
+            print("Sorry, please enter either 'R', 'B', 'L'")
             
 def welcome_tutor(tutor):
-    print("Welcome,", tutor.full_name, ". Would you like to review your profile information?")
-    print("Type 'R' if you would like to review your profile, or type 'B' to begin looking for tutors.")
+    print("Welcome,", tutor.full_name, "Would you like to review your profile information?")
+    print("Type 'R' if you would like to review your profile,\nType 'L' to logout")
     x = True
     while x:
         response = input("").lower().strip()
@@ -211,24 +246,32 @@ def welcome_tutor(tutor):
             print("Subjects:", subjects_s)
             print("Weboption:", tutor.weboption)
             print("Price per session:", tutor.cost)
-            print("If you would like to change any of the information shown above, type 'Y', or otherwise, type 'N'.")
-            while True:
-                response = input("").lower().strip()
-                if response == 'y':
-                    #change_information()
-                    x = False
-                    break
-                elif response == 'n':                    
-                    #find_tutors()
-                    x = False
-                    break
-                print("Sorry, please enter either 'Y' or 'N'.")
-            break
-        elif response == 'b':
-            #find_tutors()
+            
+            print("Type 'L' to logout")
+            response = input("").lower().strip()
+            if response == 'l':
+                logout()
+            
+            
+            #function to change parameters
+            #print("If you would like to change any of the information shown above, type #'Y', or otherwise, type 'N'.")
+            #while True:
+            #    response = input("").lower().strip()
+            #    if response == 'y':
+            #        #change_information()
+            #        x = False
+            #        break
+            #    elif response == 'n':                    
+            #        #find_tutee()
+            #        x = False
+            #       break
+            #    print("Sorry, please enter either 'Y' or 'N'.")
+            #break
+        elif response == 'l':
+            logout()
             break
         else:
-            print("Sorry, please enter either 'R' or 'B'.")            
+            print("Sorry, please enter either 'R' or 'L'.")            
     
 def sign_up_tutee():
     full_name, username, password = set_username_and_password()
@@ -269,12 +312,26 @@ def sign_up_tutor():
     welcome_tutor(tutor)  
 
 def login(tutees, tutors):
-    username = input("Please enter your username.")
-    password = input("Please type your password.")
+    username = input("Please enter your username:")
+    password = input("Please type your password:")
+    found = False
+    
     for tutee in tutees:
         if username == tutee.username and password == tutee.password:
+            found = True
             print("Welcome back,", username)
             welcome_tutee(tutee)
+            
+    for tutor in tutors:
+        if username == tutor.username and password == tutor.password:
+            print("Welcome back,", username)
+            found = True
+            welcome_tutor(tutor)        
+            
+    if found == False:
+        print("An account with the username,",  username, "does not exist")
+        print("Please Try Again\n\n")
+        homepage()
             
     for tutor in tutors:
         if username == tutors.username and password == tutors.password:
@@ -298,25 +355,24 @@ def homepage():
                 action = input("")
                 if action == '0':
                     x = False
-                    print("youre signing up as a tutee.")
+                    print("Signing up as a tutee.")
                     print("------------------------------------")                    
                     sign_up_tutee()                    
                     break
                 elif action == '1':
                     x = False
-                    print("youre signing up as a tutor.")   
+                    print("Signing up as a tutor.")   
                     print("------------------------------------")                    
                     sign_up_tutor()                     
                     break
                 print("Sorry, please enter either '0' or '1'")
         elif action == '1':
             #profile()
-            print("youre logging in.")            
+            print("You're Logging in.") 
+            login(tutees, tutors)
             break
         else:
             print("Sorry, please enter either '0' or '1'")
 
 homepage()
-
-
 
