f4990570bc0ab886ddbdbf93bab802d2b511cbd4
JenishaT <36098623+JenishaT@users.noreply.github.com>
2019-02-02 19:47:27 -0500

    Add files via upload

---
+++ tutee.py
@@ -0,0 +1,30 @@
+'Class for tutee objects'
+class Tutee:
+    username = ""
+    full_name = ""
+    password = ""
+    education = ""
+    grade_level = 0
+    location = ""
+    preferred_sex = ""
+    subjects = [] 
+    min_price = 0
+    max_price = 0
+    weboption = False
+    
+    def __init__(self, username, full_name, password, education, grade_level, location, preferred_sex, subjects, min_price,max_price, weboption):
+        self.username = username
+        self.full_name = full_name
+        self.password = password
+        self.education = education
+        self.grade_level = grade_level
+        self.location = location
+        self.preferred_sex = preferred_sex
+        self.subjects = subjects 
+        self.min_price = min_price
+        self.max_price = max_price 
+        self.weboption = weboption         
+        
+    
+    
+    
